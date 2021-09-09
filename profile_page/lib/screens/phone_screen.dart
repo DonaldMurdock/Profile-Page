ff9ddaa8e2c56f4ac816eab389959085a79a4020
Donald Taylor <taylordo@engr.orst.edu>
2021-09-09 15:22:08 -0600

    Edit phone number screen working properly

--- profile_page/lib/screens/phone_screen.dart
+++ profile_page/lib/screens/phone_screen.dart
@@ -1,18 +1,108 @@
 import 'package:flutter/material.dart';
 
+import '../models/user.dart';
+
 class PhoneScreen extends StatelessWidget {
-  const PhoneScreen({ Key? key }) : super(key: key);
+  const PhoneScreen({ Key? key, required this.currentUser, required this.updateInfo }) : super(key: key);
+
+  final User currentUser;
+  final void Function() updateInfo;
 
   @override
   Widget build(BuildContext context) {
+    TextEditingController fieldController = TextEditingController(text: currentUser.phoneNumber);
+
     return Scaffold(
       appBar: AppBar(
         //Empty invisible Appbar
         backgroundColor: Colors.transparent,
         elevation: 0.0
       ),
-      body: Text('This is the phone screen')
-      
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            children: [
+              headerText(),
+              Padding(
+                padding: EdgeInsets.all(30), 
+                child: inputField(fieldController)
+              )
+            ]
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 30, vertical: 100),
+            child: updateButton(fieldController, context)
+          )
+        ]
+      )
     );
+  }
+
+  Widget headerText() {
+    return Text(
+      'What\'s your phone number?',
+      style: TextStyle(
+        fontWeight: FontWeight.bold,
+        fontSize: 25.0
+      )
+    );
+  }
+
+  Widget inputField(TextEditingController fieldController) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.grey)
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: EdgeInsets.all(5), 
+            child: Text(
+              'Your phone number', 
+              style: TextStyle(
+                color: Colors.grey,
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold
+              )
+            )
+          ),
+          Padding(
+            padding: EdgeInsets.all(5), 
+            child: TextFormField(
+              controller: fieldController, 
+              decoration: InputDecoration(border: InputBorder.none),
+              style: TextStyle(
+                fontSize: 25.0,
+                fontWeight: FontWeight.bold
+              )
+
+            )
+          )
+        ]
+      )
+    );
+  }
+
+  Widget updateButton(TextEditingController fieldController, BuildContext context){
+    return Container(
+      width: double.infinity,
+      height: 60.0,
+      child: ElevatedButton(
+        onPressed: () {updatePhoneNumber(fieldController, context);}, 
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all(Colors.black)
+        ),
+        child: Text('Update')
+      )
+    );
+  }
+
+  updatePhoneNumber(TextEditingController fieldController, BuildContext context) {
+    currentUser.phoneNumber = fieldController.text;
+    print(currentUser.phoneNumber);
+    updateInfo();
+    Navigator.pop(context);
   }
 }
--- profile_page/lib/screens/profile_screen.dart
+++ profile_page/lib/screens/profile_screen.dart
@@ -20,10 +20,14 @@ class _ProfileScreenState extends State<ProfileScreen> {
   User currentUser = User(
     firstName: 'Donald',
     lastName: 'Taylor',
-    phoneNumber: '555-555-5555',
+    phoneNumber: '(555) 555-5555',
     email: 'DonaldMurdockTaylor@gmail.com',
     bio: 'Hello my name is Don. I would love to work for Approachable Geek! Here is more text so that I can test that my bio text is properly wrapping and not overflowing the screen.'
   );
+
+  void updateInfo() {
+    setState( () {} );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -33,33 +37,37 @@ class _ProfileScreenState extends State<ProfileScreen> {
         backgroundColor: Colors.transparent,
         elevation: 0.0
       ),
-      body: Center(
-        child: Column(
-          children: [
-            profileHeader(),
-            userPhoto(),
-            InfoItem(
-              title: 'Name',
-              content: currentUser.firstName + ' ' + currentUser.lastName,
-              screen: NameScreen()
-            ),
-            InfoItem(
-              title: 'Phone',
-              content: currentUser.phoneNumber,
-              screen: PhoneScreen()
-            ),
-            InfoItem(
-              title: 'Email',
-              content: currentUser.email,
-              screen: EmailScreen()
-            ),
-            InfoItem(
-              title: 'Tell us about yourself',
-              content: currentUser.bio,
-              screen: BioScreen()
-            ),
+      body: SingleChildScrollView(        //Need to make scrollable to avoid overflow when returning to screen with keyboard open
+        child: Center(
+          child: Column(
+            children: [
+              profileHeader(),
+              userPhoto(),
+              InfoItem(
+                title: 'Name',
+                content: currentUser.firstName + ' ' + currentUser.lastName,
+                screen: NameScreen()
+              ),
+              InfoItem(
+                title: 'Phone',
+                content: currentUser.phoneNumber,
+                screen: PhoneScreen(
+                  currentUser: currentUser,
+                  updateInfo: updateInfo)
+              ),
+              InfoItem(
+                title: 'Email',
+                content: currentUser.email,
+                screen: EmailScreen()
+              ),
+              InfoItem(
+                title: 'Tell us about yourself',
+                content: currentUser.bio,
+                screen: BioScreen()
+              ),
 
-          ]
+            ]
+          )
         )
       )
     );
